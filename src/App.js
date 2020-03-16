6ecdca7379e946a852084c9d850bfae8bae092b5
Matt Locklin <mattlocklin@live.co.uk>
2020-03-16 23:40:14 +0000

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components'
+
+const Header = styled.header`
+  color: red;
+  background-color: yellow;
+  
+`
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="App-header">
+      </Header>
+    </div>
+  );
+}
+
+export default App;
